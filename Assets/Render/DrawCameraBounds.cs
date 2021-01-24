093e7168a84f2ab589b56cd0d10f73edc6d63221
SatriAli <allister.wilson@gmail.com>
2021-01-24 16:52:39 +0100

    add checkerboard test sprite add pixel perfect sprite prefab add bounds drawing to RTT camera

---
+++ Assets/Render/DrawCameraBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCameraBounds : MonoBehaviour
+{
+    new public Camera camera;
+
+    private void OnDrawGizmos()
+    {
+        if (camera == null)
+            return;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(camera.aspect * camera.orthographicSize * 2, camera.orthographicSize * 2, 0));
+    }
+}
---
+++ Assets/Render/PixelPerfectSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PixelPerfectSprite : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+
+	private void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public Vector2 SnapPosition()
+	{
+		Vector3 halfSize = spriteRenderer.bounds.size * .5f;
+		Vector3 origin = transform.position - halfSize;
+		origin.x = Mathf.Round(origin.x);
+		origin.y = Mathf.Round(origin.y);
+		transform.position = origin + halfSize;
+		return origin;
+	}
+
+	// who needs performance anyway?
+	private void LateUpdate()
+	{
+		SnapPosition();
+	}
+}
